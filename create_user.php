f95c36d1906a8636690e571b9d19f8742a6a442d
john-csr <johnflorida.mcp@gmail.com>
2025-09-01 21:01:54 -0400

    Update create_user.php

--- create_user.php
+++ create_user.php
@@ -6,7 +6,7 @@ $conn = new mysqli("localhost", "your_username", "your_password", "your_db_name"
 if ($conn->connect_error) die("Connection failed");
 
 $username = "admin"; // change as needed
-$password = "Password#01"; // change as needed
+$password = "Your_password"; // change as needed
 $hash = password_hash($password, PASSWORD_DEFAULT);
 
 $stmt = $conn->prepare("INSERT INTO users (username, password_hash) VALUES (?, ?)");
